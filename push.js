ab487cda54cd8b9b739944273ca545be07960e87
atanpure <atanpure@rbbn.com>
2020-03-01 13:28:04 +0530

    Updated code  in service worker

--- push.js
+++ push.js
@@ -7,6 +7,11 @@ let vapidKeys = {
 
 push.setVapidDetails('mailto:tanpure.anjali@gmail.com', vapidKeys.publicKey, vapidKeys.privateKey);
 
-let sub = {};
-
+let sub = {
+    "endpoint":"https://fcm.googleapis.com/fcm/send/cxfdBmXIvGs:APA91bEyIb1YodZuWIHctSOyMGiWTMvp3EyFHsYmvbS6ppGzNQMPbjfH8jDbwACY50Xro7A-oEE41MrEfvtcbq2TcOHAljSJbv7x-Zq7lV07GyWzSP0uaVnDupOqHhioLQhVNdW_IiPz",
+    "expirationTime":null,
+    "keys":{
+        "p256dh":"BBrO_bb8bBd_kZ201MvUVSDWTjLWBm_v0sZ5h-y7_DHdvqJ15LRXSmRJyGHogBBCq7wuaClPzXnmo5LwraRJfXU",
+        "auth":"HZErgoekdTBmj3_KGmtAYw"}
+};
 push.sendNotification(sub, 'This is the Test Message');
